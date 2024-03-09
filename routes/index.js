918dbb9c57745904a9798914c91cd1c9c9838421
belyne <122968087+belyne@users.noreply.github.com>
2024-03-09 22:14:29 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,7 @@ const router = express.Router();
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
+router.post('/files', FilesController.postUpload);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', AuthController.getMe);
