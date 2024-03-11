5cbf2caf50b29c7d520e5fe47a244bf8e463537f
belyne <122968087+belyne@users.noreply.github.com>
2024-03-11 09:28:39 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-// server.js
-
 import express from 'express';
-import routes from './routes';
+import router from './routes/index';
+
+const port = parseInt(process.env.PORT, 10) || 5000;
 
 const app = express();
-const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use('/', routes);
+app.use('/', router);
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`server running on port ${port}`);
 });
+
+export default app;
